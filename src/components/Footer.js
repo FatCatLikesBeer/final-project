926ee459365df12a6f8ca04ebd3f19a52880acc2
Billy Bagel <itisbilly@gmail.com>
2023-11-28 12:28:55 -0800

    Got the footer looking good.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,28 +9,29 @@ function Footer() {
         <img src={Logo} />
       </div>
       <div className="quarter" id="navigation">
-          <h4>Navigation</h4>
-          <a href="/#home">Home</a>
-          <a href="/#about">About</a>
-          <a href="/#menu">Menu</a>
-          <a href="/#reservation">Reservation</a>
-          <a href="/#order">Order</a>
+        <h4>Navigation</h4>
+        <a href="/#home">Home</a>
+        <a href="/#about">About</a>
+        <a href="/#menu">Menu</a>
+        <a href="/#reservation">Reservation</a>
+        <a href="/#order">Order</a>
       </div>
       <div className="quarter" id="socialMedia">
-          <h4>Social Media</h4>
-          <a href="https://www.twitter.com" target="_blank">Twitter</a>
-          <a href="https://www.facebook.com" target="_blank">Facebook</a>
-          <a href="https://www.instagram.com" target="_blank">Instagram</a>
-          <a href="https://www.tiktok.com" target="_blank">TikTok</a>
-          <a href="https://www.youtube.com" target="_blank">YouTube</a>
+        <h4>Social Media</h4>
+        <a href="https://www.twitter.com" target="_blank">Twitter</a>
+        <a href="https://www.facebook.com" target="_blank">Facebook</a>
+        <a href="https://www.instagram.com" target="_blank">Instagram</a>
+        <a href="https://www.tiktok.com" target="_blank">TikTok</a>
+        <a href="https://www.youtube.com" target="_blank">YouTube</a>
       </div>
       <div className="quarter" id="contact">
-          <h4>Contact</h4>
-          <a href="/contact/#phone">Phone</a>
-          <a href="/contact/#text">Text</a>
-          <a href="/contact/#email">Email</a>
-          <a href="/contact/#address">Address</a>
+        <h4>Contact</h4>
+        <a href="/contact/#phone">Phone</a>
+        <a href="/contact/#text">Text</a>
+        <a href="/contact/#email">Email</a>
+        <a href="/contact/#address">Address</a>
       </div>
+      <div className="quarter"></div>
     </footer>
   );
 };
